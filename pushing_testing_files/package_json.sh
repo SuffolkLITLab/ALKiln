dba2b87b4076f8fed902210ada4c6cf822929fe1
plocket <52798256+plocket@users.noreply.github.com>
2020-10-19 14:42:31 -0400

    Push to root/ which will end up in root of dest

--- pushing_testing_files/package_json.sh
+++ pushing_testing_files/package_json.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # If folder doesn't exist, make folder
-mkdir to_push
+mkdir root
 
 # https://stackoverflow.com/questions/34885949/how-to-i-save-grep-regex-matched-result-to-a-variable
 # https://www.cyberciti.biz/faq/how-to-assign-a-grep-command-value-to-a-variable-in-linuxunix/
 # https://unix.stackexchange.com/a/229237
 
 repo_name=$(echo "$DESTINATION" | sed 's|.*\/\(.*\)$|\1|')
-sed -e 's|just_repo|'"$repo_name"'|' -e 's|user_repo|'"$DESTINATION"'|' pushing_testing_files/package.json > to_push/package.json
+sed -e 's|just_repo|'"$repo_name"'|' -e 's|user_repo|'"$DESTINATION"'|' pushing_testing_files/package.json > root/package.json
