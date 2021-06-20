0469d53421fd3350e43a3aad813995177673e225
plocket <52798256+plocket@users.noreply.github.com>
2021-06-20 08:47:21 -0400

    Convert page_data unit tests to fields tests

--- tests/unit_tests/getAllFields.test.js
+++ tests/unit_tests/getAllFields.test.js
@@ -4,16 +4,16 @@ const cheerio = require('cheerio');
 // We need jest or something, right? To do fancy stuff.
 
 const html = require('./html.fixtures.js');
-const page_data = require('./page_data.fixtures.js');
+const fields = require('./fields.fixtures.js');
 const scope = require('../../lib/scope.js');
-const getPageData = scope.getPageData;
+const getAllFields = scope.getAllFields;
 // TODO: Rearrange fixtures so related fixtures are next to each other? e.g.
-// buttons_yesno.html, buttons_yesno.page_data, buttons_yesno.table, buttons_yesno.matches
+// buttons_yesno.html, buttons_yesno.fields, buttons_yesno.table, buttons_yesno.matches
 // May make more files both to make and to import, but may be more useful
 // for editing, maintaining, and clarity.
 
 
-/* Test `scope.getPageData()`. Discuss: If we do a good job outlining
+/* Test `scope.getAllFields()`. Discuss: If we do a good job outlining
 * what behvior the main tests cover, then we might not need to test
 * each individual function separately.
 * 
@@ -29,8 +29,8 @@ const getPageData = scope.getPageData;
 // behavior in sub-functions if they are not tested independently
 it(`creates the right data for standard fields`, async function() {
   // 18 fields (03/15/21)
-  let result = await getPageData( scope, { html: html.standard });
-  expect( result ).to.deep.equal( page_data.standard );
+  let result = await getAllFields( scope, { html: html.standard });
+  expect( result ).to.deep.equal( fields.standard );
 });
 
 
@@ -38,8 +38,8 @@ it(`creates the right data for standard fields`, async function() {
 // Simple show if fields - no proxies
 // ============================
 it(`creates the right data for 'show if' fields`, async function() {
-  let result = await getPageData( scope, { html: html.show_if });
-  expect( result ).to.deep.equal( page_data.show_if );
+  let result = await getAllFields( scope, { html: html.show_if });
+  expect( result ).to.deep.equal( fields.show_if );
 });
 
 
@@ -48,26 +48,26 @@ it(`creates the right data for 'show if' fields`, async function() {
 // ============================
 it(`creates the right data for one continue button`, async function() {
   // `continue button field:`
-  let result = await getPageData( scope, { html: html.button_continue });
-  expect( result ).to.deep.equal( page_data.button_continue );
+  let result = await getAllFields( scope, { html: html.button_continue });
+  expect( result ).to.deep.equal( fields.button_continue );
 });
 
 it(`creates the right data for yesnomaybe buttons`, async function() {
   // `yesnomaybe:`
-  let result = await getPageData( scope, { html: html.buttons_yesnomaybe });
-  expect( result ).to.deep.equal( page_data.buttons_yesnomaybe );
+  let result = await getAllFields( scope, { html: html.buttons_yesnomaybe });
+  expect( result ).to.deep.equal( fields.buttons_yesnomaybe );
 });
 
 it(`creates the right data for other mutiple choice continue buttons`, async function() {
   // `field:` and `buttons:`
-  let result = await getPageData( scope, { html: html.buttons_other });
-  expect( result ).to.deep.equal( page_data.buttons_other );
+  let result = await getAllFields( scope, { html: html.buttons_other });
+  expect( result ).to.deep.equal( fields.buttons_other );
 });
 
 it(`creates the right data for an event action button`, async function() {
   // `field:` and `action buttons:`
-  let result = await getPageData( scope, { html: html.buttons_event_action });
-  expect( result ).to.deep.equal( page_data.buttons_event_action );
+  let result = await getAllFields( scope, { html: html.buttons_event_action });
+  expect( result ).to.deep.equal( fields.buttons_event_action );
 });
 
 
@@ -76,23 +76,23 @@ it(`creates the right data for an event action button`, async function() {
 // ============================
 // x[i].name.first
 it(`creates the right data for a multi-proxy name (x[i])`, async function() {
-  let result = await getPageData( scope, { html: html.proxies_xi });
-  expect( result ).to.deep.equal( page_data.proxies_xi );
+  let result = await getAllFields( scope, { html: html.proxies_xi });
+  expect( result ).to.deep.equal( fields.proxies_xi );
 });
 
 // Multiple proxies by the same name are on the list (because of a loop)
 // x[i].name.first
 it(`creates the right data for a multi-proxy name (x[i]) again for consistency`, async function() {
   // same data as `proxies_xi`, but have this here for consistency
-  let result = await getPageData( scope, { html: html.proxies_multi });
-  expect( result ).to.deep.equal( page_data.proxies_multi );
+  let result = await getAllFields( scope, { html: html.proxies_multi });
+  expect( result ).to.deep.equal( fields.proxies_multi );
 });
 
 // your_past_benefits[i].still_receiving
 // your_past_benefits['State Veterans Benefits'].still_receiving
 it(`creates the right data for a proxy name when a non-match comes after a match`, async function() {
-  let result = await getPageData( scope, { html: html.proxies_non_match });
-  expect( result ).to.deep.equal( page_data.proxies_non_match );
+  let result = await getAllFields( scope, { html: html.proxies_non_match });
+  expect( result ).to.deep.equal( fields.proxies_non_match );
 });
 
 
@@ -101,8 +101,8 @@ it(`creates the right data for a proxy name when a non-match comes after a match
 // ============================
 it(`creates the right data for a signature field`, async function() {
   // `field:` and `action buttons:`
-  let result = await getPageData( scope, { html: html.signature });
-  expect( result ).to.deep.equal( page_data.signature );
+  let result = await getAllFields( scope, { html: html.signature });
+  expect( result ).to.deep.equal( fields.signature );
 });
 
 
@@ -111,8 +111,8 @@ it(`creates the right data for a signature field`, async function() {
 // ============================
 it(`creates the right data for a 'choices:' field`, async function() {
   // `field:` and `choices:`
-  let result = await getPageData( scope, { html: html.choices });
-  expect( result ).to.deep.equal( page_data.choices );
+  let result = await getAllFields( scope, { html: html.choices });
+  expect( result ).to.deep.equal( fields.choices );
 });
 
 
@@ -128,36 +128,7 @@ it(`creates the right data for a 'choices:' field`, async function() {
 // ```
 it(`creates the right data for a 'choices:' field`, async function() {
   // `field:` and `choices:`
-  let result = await getPageData( scope, { html: html.object_dropdown });
-  expect( result ).to.deep.equal( page_data.object_dropdown );
+  let result = await getAllFields( scope, { html: html.object_dropdown });
+  expect( result ).to.deep.equal( fields.object_dropdown );
   // console.log( JSON.stringify(result) );
 });
-
-
-// /*
-// Fields:
-// - Checkboxes (multiple choice)
-//   Story table: var names and chioces separately in the table, as well as value to set to true/false
-//   html: have var names and choice names both in their `input[name]` attribute.
-// - Checkboxes (yes/no)
-//   Story table: var name & set to. NO choice? Or maybe flip those last two? Which would be most useful?
-//   html: `name` = var_name, True/False for `value`
-// - Buttons (continue)
-//   Story table: is var_name all that's needed? Also need 'value'? Do these even matter at all?
-//   html: var name = `name`, value = 'True'
-// - Buttons (other/multi)
-//   Story table: var name, value (set_to) needed
-//   html: var name is `name` attr, value is `value` attr
-// - Buttons (yesnomaybe)
-//   Story table: var name, value (set_to) needed
-//   html: var name = name. `value` attr: True, False, None
-// - Radio object_radio
-//   html: `value` attr has no value, `name` attr is the var name
-
-// ===============
-// Questions:
-// - Do var names contain doubly encoded stuff if they have
-// keys. e.g. `foo[B'encoded'][B'choice_encoded']`. I think not because
-// we whould have run into them by now. I think we've run into those and
-// they've been fine.
-// */
--- tests/unit_tests/getMatchingRows.test.js
+++ tests/unit_tests/getMatchingRows.test.js
@@ -1,131 +1,140 @@
-const chai = require('chai');
-const expect = chai.expect;
+// const chai = require('chai');
+// const expect = chai.expect;
 
 
-const tables = require('./tables.fixtures.js');
-const page_data = require('./page_data.fixtures.js');
-const matches = require('./matches.fixtures.js');
-const scope = require('../../lib/scope.js');
-const getMatchingRows = scope.getMatchingRows;
+// const tables = require('./tables.fixtures.js');
+// const fields = require('./fields.fixtures.js');
+// const matches = require('./matches.fixtures.js');
+// const scope = require('../../lib/scope.js');
+// const getMatchingRows = scope.getMatchingRows;
 
 
-// ============================
-// Standard fields - no proxies, no showifs.
-// ============================
-// TODO: Add more complex fields. E.g `object_checkboxes` and dropdown with `object`.
-it("matches the right table and field rows for standard fields", async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.standard, story_table: tables.standard });
-  expect( result ).to.deep.equal( matches.standard );
-  expect( Object.keys( scope.report ).length ).to.equal( 1 );
-});
+// // ============================
+// // Standard fields - no proxies, no showifs.
+// // ============================
+// // TODO: Add more complex fields. E.g `object_checkboxes` and dropdown with `object`.
+// describe("When given a field and table rows for standard fields", async function() {
+//   for ( let test_i = 0; test_i < matches.standard.length; test_i++ ) {
+//     let field = fields.standard[ test_i ];
+//     let curr_matches = matches.standard[ test_i ];
+//     it( `finds the right matches for ${ field.selector }`, async function() {
+//       let result = await getMatchingRows( scope, { field, var_data: tables.standard });
+//       expect( result ).to.deep.equal( curr_matches );
+//     });
+//   }
+// });
 
 
-// ============================
-// Simple show if fields - no proxies
-// ============================
-it("matches the right table and field rows for simple show if fields", async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.show_if, story_table: tables.show_if });
-  expect( result ).to.deep.equal( matches.show_if );
-});
+// // // ============================
+// // // Simple show if fields - no proxies
+// // // ============================
+// // it("matches the right table and field rows for simple show if fields", async function() {
+// //   let result = await getMatchingRows( scope, { fields: fields.show_if, var_data: tables.show_if });
+// //   expect( result ).to.deep.equal( matches.show_if );
+// // });
 
 
-// ============================
-// Buttons
-// ============================
-// `continue button field:`
-it("matches the right table and field rows for one continue button", async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.button_continue, story_table: tables.button_continue });
-  expect( result ).to.deep.equal( matches.button_continue );
-});
+// // // ============================
+// // // Buttons
+// // // ============================
+// // // `continue button field:`
+// // it("matches the right table and field rows for one continue button", async function() {
+// //   let result = await getMatchingRows( scope, { fields: fields.button_continue, var_data: tables.button_continue });
+// //   expect( result ).to.deep.equal( matches.button_continue );
+// // });
 
-// `yesnomaybe:`
-it("matches the right table and field rows for yesnomaybe buttons", async function() {
-  let result1 = await getMatchingRows( scope, { page_data: page_data.buttons_yesnomaybe, story_table: tables.buttons_yesnomaybe_yes });
-  expect( result1 ).to.deep.equal( matches.buttons_yesnomaybe_yes );
-  
-  let result2 = await getMatchingRows( scope, { page_data: page_data.buttons_yesnomaybe, story_table: tables.buttons_yesnomaybe_no });
-  expect( result2 ).to.deep.equal( matches.buttons_yesnomaybe_no );
-  
-  let result3 = await getMatchingRows( scope, { page_data: page_data.buttons_yesnomaybe, story_table: tables.buttons_yesnomaybe_none });
-  expect( result3 ).to.deep.equal( matches.buttons_yesnomaybe_none );
-});
+// // // `yesnomaybe:`
+// // it("matches the right table and field rows for yesnomaybe 'yes' buttons", async function() {
+// //   let result1 = await getMatchingRows( scope, { fields: fields.buttons_yesnomaybe, var_data: tables.buttons_yesnomaybe_yes });
+// //   expect( result1 ).to.deep.equal( matches.buttons_yesnomaybe_yes );
+// // });
+// // it("matches the right table and field rows for yesnomaybe 'no' buttons", async function() {
+// //   let result2 = await getMatchingRows( scope, { fields: fields.buttons_yesnomaybe, var_data: tables.buttons_yesnomaybe_no });
+// //   expect( result2 ).to.deep.equal( matches.buttons_yesnomaybe_no );
+// // });
+// // it("matches the right table and field rows for yesnomaybe 'maybe' buttons", async function() {
+// //   let result3 = await getMatchingRows( scope, { fields: fields.buttons_yesnomaybe, var_data: tables.buttons_yesnomaybe_none });
+// //   expect( result3 ).to.deep.equal( matches.buttons_yesnomaybe_none );
+// // });
 
-// `field:` and `buttons:`
-it("matches the right table and field rows for other mutiple choice continue buttons", async function() {
-  let result1 = await getMatchingRows( scope, { page_data: page_data.buttons_other, story_table: tables.buttons_other_1 });
-  expect( result1 ).to.deep.equal( matches.buttons_other_1 );
-  
-  let result2 = await getMatchingRows( scope, { page_data: page_data.buttons_other, story_table: tables.buttons_other_2 });
-  expect( result2 ).to.deep.equal( matches.buttons_other_2 );
-  
-  let result3 = await getMatchingRows( scope, { page_data: page_data.buttons_other, story_table: tables.buttons_other_3 });
-  expect( result3 ).to.deep.equal( matches.buttons_other_3 );
-});
+// // // `field:` and `buttons:`
+// // it("matches the right table and field rows for other mutiple choice continue buttons, choice 1", async function() {
+// //   let result1 = await getMatchingRows( scope, { fields: fields.buttons_other, var_data: tables.buttons_other_1 });
+// //   expect( result1 ).to.deep.equal( matches.buttons_other_1 );
+// // });
+// // it("matches the right table and field rows for other mutiple choice continue buttons, choice 2", async function() {
+// //   let result2 = await getMatchingRows( scope, { fields: fields.buttons_other, var_data: tables.buttons_other_2 });
+// //   expect( result2 ).to.deep.equal( matches.buttons_other_2 );
+// // });
+// // it("matches the right table and field rows for other mutiple choice continue buttons, choice 3", async function() {
+// //   let result3 = await getMatchingRows( scope, { fields: fields.buttons_other, var_data: tables.buttons_other_3 });
+// //   expect( result3 ).to.deep.equal( matches.buttons_other_3 );
+// // });
 
-// `field:` and `action buttons:`
-it(`matches the right table and field rows for an action button`, async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.buttons_event_action, story_table: tables.buttons_event_action });
-  expect( result ).to.deep.equal( matches.buttons_event_action );
-});
-
-
-// ============================
-// Proxy vars (x, i, j, ...)
-// ============================
-// x[i].name.first
-it(`matches the right table and field rows for a multi-proxy name (x[i])`, async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.proxies_xi, story_table: tables.proxies_xi });
-  expect( result ).to.deep.equal( matches.proxies_xi );
-});
-
-// Multiple proxies by the same name are on the list (because of a loop)
-// x[i].name.first
-it(`matches the right table and field rows for mulitple rows with the same proxies`, async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.proxies_multi, story_table: tables.proxies_multi });
-  expect( result ).to.deep.equal( matches.proxies_multi );
-});
-
-// your_past_benefits[i].still_receiving
-// your_past_benefits['State Veterans Benefits'].still_receiving
-// Non-match comes after a match
-it(`matches the right table and field rows for a proxy name when a non-match comes after a match`, async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.proxies_non_match, story_table: tables.proxies_non_match });
-  expect( result ).to.deep.equal( matches.proxies_non_match );
-  // console.log( JSON.stringify( result ) );
-});
+// // // `field:` and `action buttons:`
+// // it(`matches the right table and field rows for an action button`, async function() {
+// //   let result = await getMatchingRows( scope, { fields: fields.buttons_event_action, var_data: tables.buttons_event_action });
+// //   expect( result ).to.deep.equal( matches.buttons_event_action );
+// // });
 
 
-// ============================
-// Signature
-// ============================
-it(`matches the right table and field rows for a signature field`, async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.signature, story_table: tables.signature });
-  expect( result ).to.deep.equal( matches.signature );
-});
+// // // ============================
+// // // Proxy vars (x, i, j, ...)
+// // // ============================
+// // // x[i].name.first
+// // it(`matches the right table and field rows for a multi-proxy name (x[i])`, async function() {
+// //   let result = await getMatchingRows( scope, { fields: fields.proxies_xi, var_data: tables.proxies_xi });
+// //   expect( result ).to.deep.equal( matches.proxies_xi );
+// // });
+
+// // // Multiple proxies by the same name are on the list (because of a loop)
+// // // x[i].name.first
+// // it(`matches the right table and field rows for mulitple rows with the same proxies`, async function() {
+// //   let result = await getMatchingRows( scope, { fields: fields.proxies_multi, var_data: tables.proxies_multi });
+// //   expect( result ).to.deep.equal( matches.proxies_multi );
+// // });
+
+// // // your_past_benefits[i].still_receiving
+// // // your_past_benefits['State Veterans Benefits'].still_receiving
+// // // Non-match comes after a match
+// // it(`matches the right table and field rows for a proxy name when a non-match comes after a match`, async function() {
+// //   let result = await getMatchingRows( scope, { fields: fields.proxies_non_match, var_data: tables.proxies_non_match });
+// //   expect( result ).to.deep.equal( matches.proxies_non_match );
+// //   // console.log( JSON.stringify( result ) );
+// // });
 
 
-// ============================
-// `choices:`
-// ============================
-// `field:` and `choices:`
-it(`matches the right table and field rows for a 'choices:' specifier`, async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.choices, story_table: tables.choices });
-  expect( result ).to.deep.equal( matches.choices );
-});
+// // // ============================
+// // // Signature
+// // // ============================
+// // it(`matches the right table and field rows for a signature field`, async function() {
+// //   let result = await getMatchingRows( scope, { fields: fields.signature, var_data: tables.signature });
+// //   expect( result ).to.deep.equal( matches.signature );
+// // });
 
 
-// ============================
-// dropdowns created with objects
-// ============================
-// ```
-// - Something: some_var
-//   datatype: object
-//   object labeler: |
-//     lambda y: y.short_label()```
-//   choices: some_obj
-// ```
-it(`matches the right table and field rows for a dropdown created with objects`, async function() {
-  let result = await getMatchingRows( scope, { page_data: page_data.object_dropdown, story_table: tables.object_dropdown });
-  expect( result ).to.deep.equal( matches.object_dropdown );
-});
+// // // ============================
+// // // `choices:`
+// // // ============================
+// // // `field:` and `choices:`
+// // it(`matches the right table and field rows for a 'choices:' specifier`, async function() {
+// //   let result = await getMatchingRows( scope, { fields: fields.choices, var_data: tables.choices });
+// //   expect( result ).to.deep.equal( matches.choices );
+// // });
+
+
+// // // ============================
+// // // dropdowns created with objects
+// // // ============================
+// // // ```
+// // // - Something: some_var
+// // //   datatype: object
+// // //   object labeler: |
+// // //     lambda y: y.short_label()```
+// // //   choices: some_obj
+// // // ```
+// // it(`matches the right table and field rows for a dropdown created with objects`, async function() {
+// //   let result = await getMatchingRows( scope, { fields: fields.object_dropdown, var_data: tables.object_dropdown });
+// //   expect( result ).to.deep.equal( matches.object_dropdown );
+// // });
 
