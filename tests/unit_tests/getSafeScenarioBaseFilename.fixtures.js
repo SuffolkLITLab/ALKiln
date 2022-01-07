84689c4ad53ac8619fe0f311d84fa9cd8b8b4fb0
niharikasingh <niharikasingh@users.noreply.github.com>
2022-01-07 14:33:21 -0500

    Separate Scenario filenames, ids, and report headers for v3 (#382)
    * Add test to check that numbers don't get stripped
    
    * Fix report headings
    
    * Make report heading match what the test requies
    
    * Separate Scenario report heading vs. filename vs scenario id (#463)
    
    * Separate Scenario report heading vs. filename vs scenario id
    
    Basically, instead of putting numbers in scenario headers and/or
    adjusting tests to allow those to pass, I'm trying to make a more
    extensive change to how we're managing that whole system.
    
    Adds base-scenario-file-name creation and adds tests for that too.
    Uses "-" as separator for major items as long as it doesn't
    break our .gitignore and artifact-getting name matching in the
    workflows. A change in that
    will have to wait for a breaking version.
    
    * Remove unused function
    
    * Don't change file name parts unnecessarily
    
    Co-authored-by: plocket <52798256+plocket@users.noreply.github.com>
    Co-authored-by: k3KAW8Pnf7mkmdSMPHz27 <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>

---
+++ tests/unit_tests/getSafeScenarioBaseFilename.fixtures.js
@@ -0,0 +1,12 @@
+let names = {};
+
+names.with_spaces = `a tests description`;
+names.tag1 = `tag1`;
+names.tag2 = `tag2`;
+
+names.non_english_chars = `是汉字`;
+
+names.colon_input = `invalid:colon`;
+names.colon_output = `invalid_colon`;
+
+module.exports = names;
