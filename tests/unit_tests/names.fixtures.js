963dc17719f947d4e3996b061108fb75d58e0290
plocket <52798256+plocket@users.noreply.github.com>
2022-06-22 20:05:26 -0400

    Adjust download,error screenshot, scenario filepaths. See more * change download filepath * adjust format of scenario-based filename with page id and date * update unit tests to match * save two copies of an error screenshot

--- tests/unit_tests/names.fixtures.js
+++ tests/unit_tests/names.fixtures.js
@@ -3,16 +3,16 @@ let names = {};
 names.base_filename = `a_safe_id`;
 
 names.chinese_input = `是汉字`;
-names.chinese_output = `是汉字_`;
+names.chinese_output = `是汉字`;
 
 names.no_underscore_input = `ends_in_no_underscore`;
-names.no_underscore_output = `ends_in_no_underscore_`;
+names.no_underscore_output = `ends_in_no_underscore`;
 
 names.one_underscore_input = `ends_in_one_underscore_`;
-names.one_underscore_output = `ends_in_one_underscore__`;
+names.one_underscore_output = `ends_in_one_underscore_`;
 
 names.two_underscores_input = `ends_in_two_underscores__`;
-names.two_underscores_output = `ends_in_two_underscores___`;
+names.two_underscores_output = `ends_in_two_underscores__`;
 
 names.empty_string_input = ``;
 names.empty_string_output = ``;
@@ -21,15 +21,15 @@ names.undefined_input;
 names.undefined_output = ``;
 
 names.colon_input = `invalid:colon`;
-names.colon_output = `invalid_colon_`;
+names.colon_output = `invalid_colon`;
 
 names.slash_input = `invalid/slash`;
-names.slash_output = `invalid_slash_`;
+names.slash_output = `invalid_slash`;
 
 names.two_consecutive_invalid_input = `two_consecutive//invalid`;
-names.two_consecutive_invalid_output = `two_consecutive__invalid_`;
+names.two_consecutive_invalid_output = `two_consecutive__invalid`;
 
 names.numerical_input = `scenario_w1th_number00`;
-names.numerical_output = `scenario_w1th_number00_`;
+names.numerical_output = `scenario_w1th_number00`;
 
 module.exports = names;
