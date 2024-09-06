e2a39823dc3e6cff08ce78a1ae41b1c87cff4e19
plocket <52798256+plocket@users.noreply.github.com>
2024-09-06 16:45:24 -0400

    Change log methods signature, use fs.append, add stdout...
    - Add report_log.txt
    - Implement action output (untested)
    - Implement logs in unit tests
    
    Co-authored-by: will-morningstar <mxwillmorningstar@gmail.com>

--- tests/unit_tests/normalizeTable.test.js
+++ tests/unit_tests/normalizeTable.test.js
@@ -7,7 +7,7 @@ const tables = require('./tables.fixtures.js');
 const scope = require('../../lib/scope.js');
 
 const Log = require(`../../lib/utils/log.js`);
-const log = new Log({ context: `unit_tests normalizeTable` });
+const log = new Log({ path: `_alkiln-misc_artifacts`, context: `unit_tests normalizeTable` });
 scope.set_log({ log_obj: log });
 
 const normalizeTable = scope.normalizeTable;
